341a819f9325e025fb7bd27dd4cfb27739cf961e
Hrodberht <hrodberht.stefanescu@gmail.com>
2022-09-07 09:29:58 +0100

    Added event listener for the form

--- app.js
+++ app.js
@@ -4,3 +4,18 @@ const url =
 const formDOM = document.querySelector(".form");
 const inputDOM = document.querySelector(".form-input");
 const resultsDOM = document.querySelector(".results");
+
+formDOM.addEventListener("submit", (e) => {
+  e.preventDefault();
+  const value = inputDOM.value;
+  if (!value) {
+    resultsDOM.innerHTML =
+      '<div class="error">Please enter valid search term.</div>';
+    return;
+  }
+  fetchPages(value);
+});
+
+const fetchPages = async (searchValue) => {
+  console.log(searchValue);
+};
